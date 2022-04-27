ebd0dbb075f3d7a22bb5ae7f1b2a5bd229797a9d
Jason Bracco <bracco.jason@gmail.com>
2022-04-26 22:30:51 -0400

    commit tree bfcb87dd1e17c138867895856f5e54b5120093f6

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.round(Math.random()*10)
+
+let num3 = 40
+let num4 = 9
+let mod = num3 % num4
+
+let max = (Math.max(20, 4, 19, 18, 3))
+
+
+
+
+
+
+
+
